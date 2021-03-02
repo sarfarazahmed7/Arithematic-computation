1f24cca8ddadb25e7fe25b417b98d152672cc141
sarfarazahmed7 <ahmed.sarfraraz123@gmail.com>
2021-03-02 09:46:01 +0530

    Added:Compute a*b+c

--- arithmaticcomputation.sh
+++ arithmaticcomputation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Enter the three inputs :: "
+echo "enter the numbers for arthmetic computation"
 read a b c
 
-result1=$(($a+$b+$c))
-echo $result1
+result2=$(($a*$b+$c))
+echo $result2
