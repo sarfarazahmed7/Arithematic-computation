af3b8f33c0f624d62f582fc53cbfcfcb066392e5
sarfarazahmed7 <ahmed.sarfraraz123@gmail.com>
2021-03-03 10:31:57 +0530

    Added:store the result in dictionary

--- arithmaticcomputation.sh
+++ arithmaticcomputation.sh
@@ -3,6 +3,9 @@
 echo "enter the numbers for arthmetic computation"
 read a b c
 
+result1=$(($a+$b+$c))
+echo $result1
+
 result2=$(($a*$b+$c))
 echo $result2
 
@@ -11,3 +14,12 @@ echo $result3
 
 result4=$(($a%$b+$c))
 echo $result4
+
+declare -A ar
+
+ar[UC1]=$result1
+ar[UC2]=$result2
+ar[UC3]=$result3
+ar[UC4]=$result4
+
+echo ${ar[*]}
